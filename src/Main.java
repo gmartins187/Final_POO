3ce670f813708e5b43cb8325c8395c2bb66a352b
gmartins187 <138521631+gmartins187@users.noreply.github.com>
2025-05-12 23:12:31 +0100

    experiment

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Commands();
-        String ok = "ok";
     }
 
     private static void Commands() {
