f725999048e7bf8bae049394e606138d00376102
dreca <andre.jordao.amante@gmail.com>
2025-05-13 12:07:56 +0100

    alterei nada

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Commands());
+        int i;
     }
 
     private static int Commands() {
